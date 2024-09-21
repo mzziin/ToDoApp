7a2b467d3c911a21a575fcc50669467f7d2f1082
Mohammed Mazin <muhammedmazin38@gmail.com>
2024-09-21 16:25:13 +0530

    Initial Commit

---
+++ Todo.Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Models
+{
+    public class Task
+    {
+        [Key]
+        int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+
+        public string? Title { get; set; }
+        [MaxLength(250)]
+        public string? Description { get; set; }
+        [Required]
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+    }
+}
